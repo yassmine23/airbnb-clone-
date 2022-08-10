eac4eb1fb4014e2ca31bf2f868bb5f1e6cd7cb80
yassmine23 <yasminessam827@gmail.com>
2022-08-10 16:42:37 +0200

    add map features

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 
 import React, { useEffect } from "react";
 import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Stack from "@mui/material/Stack";
+
 
 import "./App.css";
 
 import Header from "./components/Header";
 import OptionsTab from "./components/OptionsTab";
-
-
-
-import HeaderDet from "./components/header/headerdet";
-import Home from "./components/home/home";
-import UnderHome from "./components/underHome/underHome";
-import UpperFooter from "./components/UperFooter/UperFooter";
-import FooterDet from "./components/footer/Footerdet";
-import LastFooter from "./components/lastFooter/lastFooter";
 
 import { BrowserRouter as Router, Switch, Route, Link, Routes } from "react-router-dom";
 import HomePage from './pages/HomePage';
--- src/components/chart.js
+++ src/components/chart.js
@@ -12,19 +12,47 @@ import { BarSeries } from '@devexpress/dx-react-chart';
 
 
 const data = [
-  { year: '2011', maxPrice: 100 },
-  { year: '2012', maxPrice: 160 },
-  { year: '2016', maxPrice: 100 },
-  { year: '2013', maxPrice: 90 },
-  { year: '2014', maxPrice: 100 },
-  { year: '2015', maxPrice: 130 },
-  { year: '2016', maxPrice: 100 },
-  { year: '2017', maxPrice: 150 },
-  { year: '2018', maxPrice: 190 },
-  { year: '2019', maxPrice: 220 },
-  { year: '2020', maxPrice: 250 },
-  { year: '2021', maxPrice: 320 },
-  { year: '2022', maxPrice: 300 },
+  
+ 
+  { year: '.', maxPrice: 20 },
+  { year: '2', maxPrice: 20 },
+  { year: '3', maxPrice: 30 },
+  { year: '4', maxPrice: 80 },
+  { year: '5', maxPrice: 120 },
+  { year: '6', maxPrice: 160 },
+  { year: '7', maxPrice: 140 },
+  { year: '8', maxPrice: 200 },
+  { year: '9', maxPrice: 140 },
+  { year: '10', maxPrice: 130 },
+  { year: '11', maxPrice: 100 },
+  { year: '12', maxPrice: 50 },
+  { year: '13', maxPrice: 60 },
+  { year: '14', maxPrice: 50 },
+  { year: '15', maxPrice: 20 },
+  { year: '16', maxPrice: 20 },
+  { year: '17', maxPrice: 10 },
+  { year: '18', maxPrice: 5 },
+  { year: '19', maxPrice: 5 },
+  { year: '20', maxPrice: 10 },
+  { year: '21', maxPrice: 15 },
+  { year: '22', maxPrice: 10 },
+  { year: '23', maxPrice: 10 },
+  { year: '24', maxPrice: 5 },
+  { year: '25', maxPrice: 5 },
+  { year: '26', maxPrice: 10 },
+  { year: '27', maxPrice: 10 },
+  { year: '28', maxPrice: 20 },
+  { year: '29', maxPrice: 10 },
+  { year: '30', maxPrice: 10 },
+  { year: '31', maxPrice: 5 },
+  { year: '32', maxPrice: 5 },
+  { year: '33', maxPrice: 15 },
+  { year: '34', maxPrice: 5 },
+  { year: '35', maxPrice: 10 },
+  { year: '36', maxPrice: 10 },
+  { year: '37', maxPrice: 10 },
+  { year: '38', maxPrice: 10 },
+  { year: '39', maxPrice: 20 },
   
 ];
 
@@ -44,10 +72,10 @@ export default class ChartModal extends React.PureComponent {
       <Paper >
         <Chart 
           data={chartData}
-          height={200}
+          height={150}
         >
           <ArgumentAxis />
-          <ValueAxis max={350} />
+          <ValueAxis max={250} />
 
           <BarSeries
             valueField="maxPrice"
@@ -55,7 +83,6 @@ export default class ChartModal extends React.PureComponent {
             color="gray"
 
           />
-          {/* <Title text="World maxPrice" /> */}
           <Animation />
         </Chart>
       </Paper>
