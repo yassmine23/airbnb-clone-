9df80f695df12a149d3694a38d5be8c3eea5f841
yassmine23 <yasminessam827@gmail.com>
2022-08-15 17:52:31 +0200

    card edits

--- src/components/card.js
+++ src/components/card.js
@@ -33,52 +33,63 @@ export default function MainCard() {
         <div className="row ">
 
 {data.map((dat,ky)=><Card className="col-md-3  col-sm-1 border-0 "  key={ky} >
-          <IconButton  aria-label="add an alarm" size="large" sx={{width:40, position:'absolute', top:10, left:230,zIndex:5}}>
+          <IconButton   size="large" sx={{width:40, position:'absolute', top:10, left:230,zIndex:5}}>
   <FavoriteBorderIcon  fontSize="inherit" style={{color:'white'}}/>
 </IconButton>
             <Carousel
               // activeIndex={index}
               // onSelect={handleSelect}
-              // interval={50000}
-              // className="w-100"
+              interval={50000}
             >
               
               <Carousel.Item>
                 <img  
-                  className="d-block rounded wdth" 
+                  className="d-block rounded img-fluid" 
                   src={dat.Url}
                   alt="First slide"
+                  width={640}
                 />
               </Carousel.Item>
               <Carousel.Item>
                 <img
-                  className="d-block  rounded wdth"
+                  className="d-block  rounded img-fluid"
                   src={dat.Url2}
                   alt="Second slide"
+                  width={640} 
+
                 />
               </Carousel.Item>
               <Carousel.Item>
                 <img
-                  className="d-block rounded wdth"
+                  className="d-block rounded img-fluid"
                   src={dat.Url3}
                   alt="third slide"
+                  width={640} 
+
                 />
               </Carousel.Item>
               <Carousel.Item>
                 <img
-                  className="d-block rounded wdth"
+                  className="d-block rounded img-fluid"
                   src={dat.Url4}
-                  alt="third slide"
+                  alt="forth slide"
+                  width={640} 
+
                 />
               </Carousel.Item>
             </Carousel>
-
+          
+          <Link to={'/details'} style={{textDecoration:'none'}}>
             <Card.Body className="text-muted">
-              <Card.Title className="text-black ">{dat.address}</Card.Title>
+             <div className='d-flex flex-row justify-content-between text-black'>
+               <Card.Title >{dat.address}</Card.Title>
+               <div> <StarIcon fontSize="inherit"/> 5.0</div>
+               </div>
               <div >3.200 kilometers away</div>
               <div >Aug 29-sep 3</div>
               <div >${dat.price} night</div>
             </Card.Body>
+            </Link>
           </Card>
 
 )}
