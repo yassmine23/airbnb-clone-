3f3e79359fa124110898466173bccf5554768608
AhmedMaged97 <ahmedmaged.abdelhay@gmail.com>
2022-08-19 22:16:37 +0200

    handle Details

--- src/pages/HostingDetails.js
+++ src/pages/HostingDetails.js
@@ -14,6 +14,7 @@ import { useSelector } from 'react-redux';
 
 export default function HostingDetails() {
   var dataDetails = useSelector((state)=>state.requestDetail.details);
+  
   return (
     <>
       <Header />
